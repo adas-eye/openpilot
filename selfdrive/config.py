b992a4c133397c6fa2866fb65bb63b67550a2510
David Schneider <David@Schneider.fm>
2020-05-20 00:25:31 +0200

    good meme test

--- selfdrive/config.py
+++ selfdrive/config.py
@@ -16,6 +16,7 @@ class Conversions:
   #Mass
   LB_TO_KG = 0.453592
 
+#meme
 
 RADAR_TO_CENTER = 2.7   # (deprecated) RADAR is ~ 2.7m ahead from center of car
 RADAR_TO_CAMERA = 1.52   # RADAR is ~ 1.5m ahead from center of mesh frame
@@ -27,4 +28,3 @@ class UIParams:
   car_front = 2.6924 * lidar_zoom
   car_back  = 1.8796 * lidar_zoom
   car_color = 110
-
